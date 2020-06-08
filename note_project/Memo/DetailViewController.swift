da55bd999fabdda5f40a92a5c382cb90458967b0
18rd165 <18rd165@ms.dendai.ac.jp>
2020-06-08 16:45:24 +0900

    サイドバーの追加

--- note_project/Memo/DetailViewController.swift
+++ note_project/Memo/DetailViewController.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    // ----------------------  Side buttom  --------------------------
+    @IBOutlet weak var rightContainerView: UIView! {
+        didSet {
+            guard let viewController = UIStoryboard(name: "SideBar", bundle: nil).instantiateInitialViewController() else { return }
+            rightContainerView.frame = rightContainerViewInitFrame
+            rightContainerView.addSubview(viewController.view)
+        }
+    }
+    
+    private let rightContainerViewInitFrame = CGRect(x: UIScreen.main.bounds.width, y: 0,
+    width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+    
+    private let containerViewDispFrame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        for touch in touches {
+            if let view = touch.view {
+                transitionSlideViewController(tag: view.tag)
+            }
+        }
+    }
+    
+    func transitionSlideViewController(tag: Int) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.rightContainerView.frame = self.rightContainerViewInitFrame
+        })
+    }
+    
+    
+    @IBAction func slideButtom(_ sender: Any) {
+        rightContainerView.frame = rightContainerViewInitFrame
+        UIView.animate(withDuration: 0.3, animations: {
+            self.rightContainerView.frame = self.containerViewDispFrame
+        })
+    }
+    
+   // -----------------------------------------------------------------------------
+    
+    
     @IBOutlet weak var memoTextView: UITextView!
     
     var selectedRow:Int!
---
+++ note_project/Memo/RightView.swift
@@ -0,0 +1,12 @@
+//
+//  Right.swift
+//  note_project
+//
+//  Created by rui on 2020/06/08.
+//  Copyright © 2020 18rd165. All rights reserved.
+//
+
+import UIKit
+
+class RightViewController: UIViewController {
+}
